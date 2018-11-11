592ddb94b3ce738b98e311f1109fc587e3c5aa16
Etienne SELLAN <git@sellan.fr>
2018-11-11 23:27:46 +0100

    Add hids

--- pype.py
+++ pype.py
@@ -23,6 +23,7 @@ import binascii
 import shutil
 import base64
 import sqlite3
+import hashlib
 
 # SETTINGS BEGIN
 url = "http://pype.sellan.fr/"
@@ -53,6 +54,7 @@ def array_to_path(path_array):
 
 
 def write_logs(message,error=False):
+    print(message)
     now = time.asctime(time.localtime(time.time()))
     logs_file = 'request.log' if error else 'error.log'
     logs_full_path = array_to_path(logs_path + [logs_file])
@@ -79,13 +81,26 @@ def database_initialisation():
     cursor = database.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     if len(cursor.fetchall()) <= 0:
-        write_logs("Database inistialisation")
-        cursor.execute("CREATE TABLE 'files' ( 'name' text NOT NULL, 'full_path' text NOT NULL, 'token' text NOT NULL, 'creation_date' bigint NOT NULL, 'checksum' text NOT NULL, 'last_check' bigint NOT NULL );")
+        write_logs("Database initialisation")
+        # Create files table
+        cursor.execute("CREATE TABLE 'files' (\
+                       'name' text NOT NULL,\
+                       'full_path' text NOT NULL,\
+                       'token' text NOT NULL,\
+                       'change_date' bigint NOT NULL,\
+                       'access_date' bigint NOT NULL,\
+                       'size' bigint NOT NULL,\
+                       'inode' bigint NOT NULL,\
+                       'links_count' bigint NOT NULL,\
+                       'checksum' text NOT NULL,\
+                       'last_check' BIGINT NOT NULL );")
+        database.commit()
 
 
 def initialisation():
     path_initialisation()
     database_initialisation()
+    hids_check()
 
 
 class request_handler(BaseHTTPRequestHandler):
@@ -222,16 +237,75 @@ def clean_files():
     now = time.time()
     # Compute the limit_date from setings
     limit_date = now - (delete_limit * 3600)
-    for file in os.listdir(array_to_path(directory)):
-        if os.path.exists(array_to_path(directory+[file])):
-            # Get informations about this file
-            stats = os.stat(array_to_path(directory+[file]))
+    for rep in os.listdir(array_to_path(directory)):
+        if os.path.exists(array_to_path(directory+[rep])):
+            # Get informations about this directory
+            stats = os.stat(array_to_path(directory+[rep]))
             timestamp = stats.st_ctime
             if timestamp < limit_date:
-                removed.append(file)
-                shutil.rmtree(array_to_path(directory+[file]))
+                removed.append(rep)
+                shutil.rmtree(array_to_path(directory+[rep]))
     if len(removed) > 0:
         write_logs("Files removed : {}".format(', '.join(removed)))
+    hids_check()
+
+
+def hids_check():
+    now = time.time()
+    limit_date = now - (delete_limit * 3600)
+    for token in os.listdir(array_to_path(directory)):
+        if os.path.exists(array_to_path(directory+[token])):
+            # Check files count in directory
+            if len(os.listdir(array_to_path(directory+[token]))) > 1:
+                write_logs("More than 1 file in {}".format(array_to_path(directory+[token])), True)
+            # Get informations about this file
+            name = os.listdir(array_to_path(directory+[token]))[0]
+            full_path = array_to_path(directory+[token,name])
+            stats = os.stat(full_path)
+            change_date = stats.st_ctime
+            access_date = stats.st_atime
+            size = stats.st_size
+            inode = stats.st_ino
+            links_count = stats.st_nlink
+            with open(full_path, 'rb') as content:
+                checksum = hashlib.sha256(content.read()).hexdigest()
+            properties = [name, full_path, token, change_date, access_date, size, inode, links_count, checksum, now]
+            properties_to_check = [name, full_path, token, change_date, size, inode, links_count, checksum]
+            cursor = database.cursor()
+            cursor.execute("SELECT name,\
+                           full_path,\
+                           token,\
+                           change_date,\
+                           size,\
+                           inode,\
+                           links_count,\
+                           checksum\
+                           FROM files\
+                           WHERE full_path='{}';".format(full_path))
+            results = cursor.fetchall()
+            if len(results) < 1:
+                write_logs("Add {} to database".format(full_path))
+                # Create files table
+                cursor.execute("INSERT INTO 'files' (\
+                               'name',\
+                               'full_path',\
+                               'token',\
+                               'change_date',\
+                               'access_date',\
+                               'size',\
+                               'inode',\
+                               'links_count',\
+                               'checksum',\
+                               'last_check'\
+                               ) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(*properties))
+                database.commit()
+            elif len(results) > 1:
+                write_logs("More than 1 result in database for {}".format(full_path), True)
+            else:
+                line = results[0]
+                for i in range(0,len(line)):
+                    if line[i] != properties_to_check[i]:
+                        write_logs("{} != {} for {}".format(line[i], properties_to_check[i], full_path), True)
 
 
 if __name__ == "__main__":
