9c63e556129e09444e84c41234fcef1c73779c0b
Etienne Sellan <git@sellan.fr>
2019-01-15 16:02:19 +0100

    Back to basics

--- pype.py
+++ pype.py
@@ -22,22 +22,26 @@ import os
 import binascii
 import shutil
 import base64
-import sqlite3
-import hashlib
 
 # SETTINGS BEGIN
-url = "http://pype.sellan.fr/"
-port = 80
-directory = "/tmp"
-delete_limit = 24  # hours
-cleaning_interval = 1  # hours
-id_length = 2  # bytes
-max_name_length = 64  # chars
-max_file_size = 52428800  # bytes
-logs_path = "/var/log"
-database = sqlite3.connect('pype.db')
+settings = {}
+settings["url"] = "http://pype.sellan.fr"
+settings["port"] = 80
+settings["directory"] = "/tmp"
+settings["delete_limit"] = 24  # hours
+settings["cleaning_interval"] = 1  # hours
+settings["id_length"] = 2  # bytes
+settings["max_name_length"] = 64  # chars
+settings["max_file_size"] = 52428800  # bytes
+settings["logs_path"] = "/var/log"
 # SETTINGS END
 
+
+def settings_initialisation():
+    settings_list = ['url', 'port', 'directory', 'delete_limit', 'cleaning_interval', 'id_length', 'max_name_length', 'max_file_size', 'logs_path']
+    for setting in settings_list:
+        if ("pype_"+setting) in os.environ:
+            settings[setting] = os.environ[("pype_"+setting)]
 
 def path_to_array(path):
     # Split path
@@ -57,50 +61,28 @@ def write_logs(message,error=False):
     print(message)
     now = time.asctime(time.localtime(time.time()))
     logs_file = 'request.log' if error else 'error.log'
-    logs_full_path = array_to_path(logs_path + [logs_file])
+    logs_full_path = array_to_path(settings["logs_path"] + [logs_file])
     with open(logs_full_path, 'a') as logs:
         logs.write("{} : {}\n".format(now, message))
 
-
 def path_initialisation():
     global directory
-    directory = path_to_array(directory)
+    directory = path_to_array(settings["directory"])
     directory.append("pype")
     # Create directory for Pype if not exist
     if not os.path.exists(array_to_path(directory)):
         os.makedirs(array_to_path(directory), 666)
     global logs_path
-    logs_path = path_to_array(logs_path)
+    logs_path = path_to_array(settings["logs_path"])
     logs_path.append("pype")
     # Create directory for Pype if not exist
     if not os.path.exists(array_to_path(logs_path)):
         os.makedirs(array_to_path(logs_path), 666)
 
 
-def database_initialisation():
-    cursor = database.cursor()
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    if len(cursor.fetchall()) <= 0:
-        write_logs("Database initialisation")
-        # Create files table
-        cursor.execute("CREATE TABLE 'files' (\
-                       'name' text NOT NULL,\
-                       'full_path' text NOT NULL,\
-                       'token' text NOT NULL,\
-                       'change_date' bigint NOT NULL,\
-                       'access_date' bigint NOT NULL,\
-                       'size' bigint NOT NULL,\
-                       'inode' bigint NOT NULL,\
-                       'links_count' bigint NOT NULL,\
-                       'checksum' text NOT NULL,\
-                       'last_check' BIGINT NOT NULL );")
-        database.commit()
-
-
 def initialisation():
+    settings_initialisation()
     path_initialisation()
-    database_initialisation()
-    hids_check()
 
 
 class request_handler(BaseHTTPRequestHandler):
@@ -127,7 +109,7 @@ class request_handler(BaseHTTPRequestHandler):
                     self.send_header('Content-type', 'text/html')
                     self.end_headers()
                     self.response = "Name: {}\nSize: {}\nCountdown: {} minute(s)\n"
-                    self.file.countdown = round((((delete_limit * 3600) + self.file.stat.st_ctime) - time.time())/60)
+                    self.file.countdown = round((((settings["delete_limit"] * 3600) + self.file.stat.st_ctime) - time.time())/60)
                     # Place data in response
                     self.response = self.response.format(path_to_array(self.file.name)[-1], self.file.stat.st_size, self.file.countdown)
                     # Send response
@@ -155,7 +137,7 @@ class request_handler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 # Send HTML page with replaced data
-                self.wfile.write(str.encode(homepage.read().replace("[url]", url)))
+                self.wfile.write(str.encode(homepage.read().replace("[url]", settings["url"])))
         return
 
     def do_PUT(self):  # For upload
@@ -165,14 +147,14 @@ class request_handler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')  # Send mime
         self.end_headers()  # Close header
         self.file_name = self.path.split("/")[-1]  # Only take the file name
-        if len(self.file_name) > max_name_length:  # Check file name length
+        if len(self.file_name) > settings["max_name_length"]:  # Check file name length
             HTML_error = "Error: Too long file name (max {} chars)\n"
-            HTML_error = HTML_error.format(max_name_length)
+            HTML_error = HTML_error.format(settings["max_name_length"])
             self.wfile.write(str.encode(HTML_error))  # Return error
             return
-        if self.file_size > max_file_size:  # Check file size
+        if self.file_size > settings["max_file_size"]:  # Check file size
             HTML_error = "Error: Too big file (max {})\n"
-            HTML_error = HTML_error.format(human_readable(max_file_size))
+            HTML_error = HTML_error.format(human_readable(settings["max_file_size"]))
             self.wfile.write(str.encode(HTML_error))  # Return error
             return
         # Read content from request
@@ -180,7 +162,7 @@ class request_handler(BaseHTTPRequestHandler):
         # Loop for generating uniq token
         while "Bad token":
             # Get random token from urandom
-            random_token = binascii.hexlify(os.urandom(id_length)).decode()
+            random_token = binascii.hexlify(os.urandom(settings["id_length"])).decode()
             # If directory not exist -> token free
             if not os.path.exists(array_to_path(directory+[random_token])):
                 break
@@ -194,21 +176,21 @@ class request_handler(BaseHTTPRequestHandler):
         current_file.write(content)
         current_file.close()
         # Return new file url to user
-        self.wfile.write(str.encode(url+random_token+"/"+self.file_name+"\n"))
+        self.wfile.write(str.encode(settings["url"]+random_token+"/"+self.file_name+"\n"))
         return
 
 
 def run_on(port):
     print("\n")
-    print("/--------------------------------\\")
-    print("|  Starting a server on port {}  |".format(port))
-    print("\\--------------------------------/")
+    print("/----------------------------\\")
+    print("|  Starting Pype on port {}  |".format(settings["port"]))
+    print("\\----------------------------/")
     print("\n")
     print("Reminder : \n")
-    print("To upload   :      curl -T file.txt {}".format(url))
-    print("To download :      curl {}/[id]/file.txt > files.txt".format(url))
+    print("To upload   :      curl -T file.txt {}".format(settings["url"]))
+    print("To download :      curl {}/[id]/file.txt > files.txt".format(settings["url"]))
     print("\n\nLogs : \n")
-    server_address = ('localhost', port)
+    server_address = ('localhost', settings["port"])
     httpd = HTTPServer(server_address, request_handler)
     httpd.serve_forever()
 
@@ -236,7 +218,7 @@ def clean_files():
     removed = []
     now = time.time()
     # Compute the limit_date from setings
-    limit_date = now - (delete_limit * 3600)
+    limit_date = now - (settings["delete_limit"] * 3600)
     for rep in os.listdir(array_to_path(directory)):
         if os.path.exists(array_to_path(directory+[rep])):
             # Get informations about this directory
@@ -247,72 +229,13 @@ def clean_files():
                 shutil.rmtree(array_to_path(directory+[rep]))
     if len(removed) > 0:
         write_logs("Files removed : {}".format(', '.join(removed)))
-    hids_check()
-
-
-def hids_check():
-    now = time.time()
-    limit_date = now - (delete_limit * 3600)
-    for token in os.listdir(array_to_path(directory)):
-        if os.path.exists(array_to_path(directory+[token])):
-            # Check files count in directory
-            if len(os.listdir(array_to_path(directory+[token]))) > 1:
-                write_logs("More than 1 file in {}".format(array_to_path(directory+[token])), True)
-            # Get informations about this file
-            name = os.listdir(array_to_path(directory+[token]))[0]
-            full_path = array_to_path(directory+[token,name])
-            stats = os.stat(full_path)
-            change_date = stats.st_ctime
-            access_date = stats.st_atime
-            size = stats.st_size
-            inode = stats.st_ino
-            links_count = stats.st_nlink
-            with open(full_path, 'rb') as content:
-                checksum = hashlib.sha256(content.read()).hexdigest()
-            properties = [name, full_path, token, change_date, access_date, size, inode, links_count, checksum, now]
-            properties_to_check = [name, full_path, token, change_date, size, inode, links_count, checksum]
-            cursor = database.cursor()
-            cursor.execute("SELECT name,\
-                           full_path,\
-                           token,\
-                           change_date,\
-                           size,\
-                           inode,\
-                           links_count,\
-                           checksum\
-                           FROM files\
-                           WHERE full_path='{}';".format(full_path))
-            results = cursor.fetchall()
-            if len(results) < 1:
-                write_logs("Add {} to database".format(full_path))
-                # Create files table
-                cursor.execute("INSERT INTO 'files' (\
-                               'name',\
-                               'full_path',\
-                               'token',\
-                               'change_date',\
-                               'access_date',\
-                               'size',\
-                               'inode',\
-                               'links_count',\
-                               'checksum',\
-                               'last_check'\
-                               ) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');".format(*properties))
-                database.commit()
-            elif len(results) > 1:
-                write_logs("More than 1 result in database for {}".format(full_path), True)
-            else:
-                line = results[0]
-                for i in range(0,len(line)):
-                    if line[i] != properties_to_check[i]:
-                        write_logs("{} != {} for {}".format(line[i], properties_to_check[i], full_path), True)
 
 
 if __name__ == "__main__":
-    server = Thread(target=run_on, args=[port])
+    server = Thread(target=run_on, args=[settings["port"]])
     server.daemon = True
     server.start()
     initialisation()
     # Launch auto cleaning interval
-    set_interval(clean_files, (cleaning_interval * 3600))
+    set_interval(clean_files, (settings["cleaning_interval"] * 3600))
     signal.pause()
