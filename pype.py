1761d7612d69ded35fdb6d31c987a88ee59ae1b7
Etienne Sellan <git@sellan.fr>
2020-04-07 22:39:14 +0200

    Fix alias file

--- pype.py
+++ pype.py
@@ -129,7 +129,7 @@ class request_handler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 with open(settings["current_directory"]+'/'+'alias.sh', 'r') as alias_file:
-                    self.wfile.write(str.encode(help_file.read().replace("[url]", settings["url"])+"\n"))
+                    self.wfile.write(str.encode(alias_file.read().replace("[url]", settings["url"])+"\n"))
             elif self.request_path[0] == "Github-ribbon.png":
                 with open(settings["current_directory"]+'/'+'Github-ribbon.png', 'rb') as image:
                     self.send_response(200)
